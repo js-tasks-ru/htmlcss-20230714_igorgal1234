6dde03e38637067b0cf63bcb47a3696de336e4e7
ihorhalchenko <ihorhalchenkoit@gmail.com>
2023-08-09 22:41:11 +0300

    Solution for lection3-modal

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,19 +1,22 @@
-(function() {
-    const close = document.getElementById('modal-close');
-    const modal = document.getElementById('modal');
-    const modalOpen = document.getElementById('modal-open');
+(function () {
+  const close = document.getElementById('modal-close');
+  const modal = document.getElementById('modal');
+  const modalOpen = document.getElementById('modal-open');
+  const overlay = document.getElementById('overlay');
 
-    if (!close || !modalOpen || !modal) {
-        return;
-    }
+  if (!close || !modalOpen || !modal) {
+    return;
+  }
 
-    modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
-        document.body.overflow = 'hidden';
-    });
+  modalOpen.addEventListener('click', () => {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+    document.body.overflow = 'hidden';
+  });
 
-    close.addEventListener('click', () => {
-        modal.style.display = 'none';
-        document.body.overflow = 'initial';
-    });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+    document.body.overflow = 'initial';
+  });
 })();
