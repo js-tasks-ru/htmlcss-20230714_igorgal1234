e4d3af0fe71931ce2119ea9350741309350efe9a
ihorhalchenko <ihorhalchenkoit@gmail.com>
2023-08-10 18:11:18 +0300

    Fixes after review

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,7 +2,6 @@
   const close = document.getElementById('modal-close');
   const modal = document.getElementById('modal');
   const modalOpen = document.getElementById('modal-open');
-  const overlay = document.getElementById('overlay');
 
   if (!close || !modalOpen || !modal) {
     return;
@@ -10,13 +9,11 @@
 
   modalOpen.addEventListener('click', () => {
     modal.style.display = 'block';
-    overlay.style.display = 'block';
-    document.body.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
   });
 
   close.addEventListener('click', () => {
     modal.style.display = 'none';
-    overlay.style.display = 'none';
-    document.body.overflow = 'initial';
+    document.body.style.overflow = 'initial';
   });
 })();
